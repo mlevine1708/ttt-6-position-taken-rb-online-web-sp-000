ea38514b1ef035f60b5587fe959f11615d71442b
Micah Levine <micah.levine@outlook.com>
2019-07-16 21:57:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
   board[index] ==  ("" or " ")
-if board[index] == "" then return false
-  if board[index] == " " then return false
-    if board[index] == nil then return false
+if board[index] == "" then return false or
+  if board[index] == " " then return false or
+    if board[index] == nil then return false or
     else return true
 end
