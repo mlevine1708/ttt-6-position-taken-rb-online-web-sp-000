4f6369170f3bec25854ace4ad78830795c6f3a71
Micah Levine <micah.levine@outlook.com>
2019-07-16 21:48:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
   board[index] ==  ("" or " ")
-  board[index] ==  ("" or " " || nil)
+  board[index] == " " || board[index] == "" || board[index]=nil
+
 end
